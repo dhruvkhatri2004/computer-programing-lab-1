f710e8c7f5a1f36144efc5c740a83adb3ace23c5
dhruvkhatri2004 <dhruvkhatri2025@gmail.com>
2025-08-12 17:47:15 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b,c;
+printf("enter 1 number/n");
+scanf("%d", &a);
+printf("enter 2 number/n");
+scanf("%d",&b);
+c=a+b;
+printf("%d+%d=%d/n",c);
+return 0;
+}
